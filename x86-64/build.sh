898b1adf5494e35eec36a2f4827e8dc06378b010
Zongfei Jing <jingzf0214@gmail.com>
2025-01-17 21:48:01 +0800

    add udpxy plugin for x64

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -40,6 +40,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-app-udpxy"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
